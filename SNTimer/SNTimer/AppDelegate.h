a326583ce870ad90a03299e4eddd31082431ea82
孙宁 <>
2017-07-27 10:01:52 +0800

    init project

---
+++ SNTimer/SNTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNTimer
+//
+//  Created by 孙宁 on 2017/7/27.
+//  Copyright © 2017年 孙宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
